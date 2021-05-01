a84c93cbb3d12a22870e9532eb6a93a85a6cb84e
PouRia <pouria.bussiness@gmail.com>
2021-05-01 10:40:52 +0430

    Project structuring to project

---
+++ src/screens/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, Layout} from '@ui-kitten/components'
+
+function Home (){
+    return (
+        <Layout style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+            <Text category='h2'>Pouria</Text>
+        </Layout>
+    )
+}
+export default Home
